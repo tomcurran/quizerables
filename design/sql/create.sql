3c7c34f104bd29e790d28d0724c7aeda73af72ef
Tom Curran <tomcurran5@gmail.com>
2012-04-02 19:07:17 +0100

    added create sql schema

---
+++ design/sql/create.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS questionnaire;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS answer;
+
+CREATE TABLE user (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password BINARY(32) NOT NULL
+);
+
+CREATE TABLE questionnaire (
+    naire_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    naire_user_id INT NOT NULL,
+    naire_title VARCHAR(255) NOT NULL,
+    naire_description TEXT,
+    naire_theme_id INT NOT NULL,
+    naire_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    naire_updated TIMESTAMP
+);
+
+CREATE TABLE question (
+    quest_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quest_naire_id INT NOT NULL,
+    quest_text TEXT NOT NULL,
+    quest_required BOOL NOT NULL
+);
+
+CREATE TABLE answer (
+    answer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    answer_quest_id INT NOT NULL,
+    answer_text TEXT,
+    answer_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
