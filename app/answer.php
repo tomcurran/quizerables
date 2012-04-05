312ce5fd50f7a6427603549d9f20f5e2788fbceb
John Norrington <johnnorro@gmail.com>
2012-04-05 18:48:40 +0100

    Added twig templating system, answer questionnaire functionality added

---
+++ app/answer.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+  'cache' => 'templates/compilation_cache',
+));
+$twig->setCache(false);
+
+require_once("DB.php");
+$db = new DB();
+
+foreach (array_keys($_POST) as $key) {
+	if (is_int($key) && isset($_POST[$key])) {
+		$db->answerQuestion($key, $_POST[$key]);
+	}
+}
+
+echo $twig->render("answer.html");
+
+?>
