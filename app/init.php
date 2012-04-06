f72d6aafe632db7b33233018d3f585a14bb8cdc5
Tom Curran <tomcurran5@gmail.com>
2012-04-06 13:04:19 +0100

    automatic class to avoid having to maually require files

--- app/init.php
+++ app/init.php
@@ -1,14 +1,37 @@
 <?php
 
-require_once 'lib/Twig/Autoloader.php';
+define('ROOT', dirname(__FILE__) . '/');
+
+$loader_paths = array(
+    '',
+    'lib/Twig/'
+);
+
+function loader($name) {
+	global $loader_paths;
+
+    $name = preg_replace('/Twig_/', '', $name, 1);
+    if (class_exists($name, false)) return;
+
+    foreach ($loader_paths as $path) {
+        $filename = ROOT . $path . $name . '.php';
+        if (file_exists($filename)){
+            require_once($filename);
+            return;
+        }
+    }
+
+}
+
+spl_autoload_register('loader');
+
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-  'cache' => 'templates/compilation_cache',
+  'cache' => 'templates/cache',
 ));
 $twig->setCache(false);
 
-require_once('DB.php');
 $db = new DB();
 
 ?>
